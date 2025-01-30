8600a4e4a6ecc39f93e0bde7330e0f13fdb0e46f
Josadack <josadaqueferreira@outlook.com>
2025-01-30 15:19:05 -0300

    :sparkles: Criação Data

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProdService } from './data/services/prod.service';
+import { ViagemModule } from './viagem/viagem.module';
+import { VeiculoModule } from './veiculo/veiculo.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { DevService } from './data/services/dev.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: DevService, //Trocar sempre que necessario, quando for local  DEVSERVICE - E quando for remoto PRODSERVICE
+      imports: [ConfigModule],
+    }),
+    ViagemModule,
+    VeiculoModule,
+    UsuarioModule,
+  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
