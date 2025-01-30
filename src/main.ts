cbd6f4ceeffd9c125533d369425373205b61a886
Victor Pestana <victorpestanavgp@gmail.com>
2025-01-30 11:28:18 -0300

    :sparkles: Implementação Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+  .setTitle('Vammo 🚗🚘')
+  .setDescription('Vammo - Projeto Integrador Grupo 03')
+  .setContact("Grupo 03","https://github.com/projetointegrador-g3/vammo","grupo03integrador@gmail.com")
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
+  
+  process.env.TZ = '-03:00';
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors()
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
+
