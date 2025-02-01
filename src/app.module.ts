c978680b54090fbc1fae1c3a896d04ce45da7ef5
Josadack <josadaqueferreira@outlook.com>
2025-02-01 01:21:33 -0300

    Conexão

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService, 
+      useClass: ProdService, 
       imports: [ConfigModule],
     }),
     ViagemModule,
