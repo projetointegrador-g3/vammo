f74a59ed79438c074aa2279e936b1b76e6902692
Grupo 03 <grupo03integrador@gmail.com>
2025-03-06 23:43:16 -0300

    Update veiculo.entity.ts

--- src/veiculo/entities/veiculo.entity.ts
+++ src/veiculo/entities/veiculo.entity.ts
@@ -40,10 +40,10 @@ export class Veiculo{
     @Column({length: 5000, nullable: false})
     observacao: string;
 
-    @IsBoolean()
-    @ApiProperty({ description: "Indica se o veículo está disponivel para viagem", default: true })
-    @Column({ type: "boolean", default: true , nullable: true})
-    disponivel: boolean;
+    @IsNotEmpty()
+    @ApiProperty()
+    @Column({length: 30, nullable: false})
+    disponivel: string;
 
     // Relacionamentos 
     @ApiProperty()
